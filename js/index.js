7a0eeb8d5c794aac7895a5e8c77f7a4c8e2aaf6a
tekihei2317 <tekihei2317@gmail.com>
2020-05-21 16:49:51 +0900

    ホーム画面のアニメーションを追加

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
 {
-  let heart=document.getElementById('heart2');
+  let heart=document.getElementById('heart');
   console.log(heart);
   let style=heart.style;
   console.log(style);
 
-  const heartHeight=560;
-  const heartWidth=600;
-  let curHeight=200;
+  const heartHeight=600;
+  const heartWidth=720;
+  let curHeight=0;
+  style.clip=`rect(${heartHeight-curHeight}px, ${heartWidth}px, ${heartHeight}px, 0px`;
 
-  console.log(`rect(${heartHeight-curHeight}px, ${heartWidth}px, ${heartHeight}px, 0px)`);
-  style.clip=`rect(${heartHeight-curHeight}px, ${heartWidth}px, ${heartHeight}px, 0px)`;
+  function addHeight(){
+    if(curHeight<heartHeight){
+      curHeight+=2;
+      style.clip=`rect(${heartHeight-curHeight}px, ${heartWidth}px, ${heartHeight}px, 0px`;
+      setTimeout(addHeight, 5);
+    }
+  }
+  addHeight();
+
 }
